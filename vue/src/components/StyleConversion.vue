aca38a456af336004e3ca336c7c217a816cb3fb7
duudf <2377584983@qq.com>
2024-05-12 16:16:38 +0800

    风格迁移前端修改

--- vue/src/components/StyleConversion.vue
+++ vue/src/components/StyleConversion.vue
@@ -1,74 +1,59 @@
 <template xmlns:v-bind="http://www.w3.org/1999/xhtml">
   <header-bar></header-bar>
-<!--   <div class="centered-container">-->
-<!--    <el-select v-model="value" placeholder="请选择要转换的图片风格" class="custom-select"  >-->
-<!--      <el-option-->
-<!--        v-for="item in options"-->
-<!--        :key="item.value"-->
-<!--        :label="item.label"-->
-<!--        :value="item.value">-->
-<!--      </el-option>-->
-<!--    </el-select>-->
-<!--  </div>-->
-
-
-
-
-  <div  style="display: flex; justify-content: center; align-items: center; flex-direction: column;">
-     <input type="file" @change="handleFileUpload" style="display: none;">
-     <button type="button" class="btn btn-primary" onclick="document.querySelector('input[type=file]').click()">
-        <span class="justify-content-center" aria-hidden="true">选择文件</span>
-     </button>
-
-
-
-   <div id="app" class="centered-container">
-    <select v-model="selectedOption" @change="handleSelectChange">
-      <option disabled value="">选择转换风格</option>
-      <option v-for="option in options" :key="option.value" :value="option.value">
-        {{ option.label }}
-      </option>
-    </select>
-<!--    <p v-if="selectedOption">当前选中的是: {{ selectedOption }}</p>-->
-  </div>
-
-<!--    <input type="file" @change="handleWeightFileUpload">-->
-    <img :src="originalImageUrl" v-if="originalImageUrl" alt="Uploaded Image">
-    <button type="button" class="btn btn-primary" @click="PassInThePicture">上传图片</button>
-
-    <!-- Loading 组件，根据 uploading 变量决定是否显示 -->
-    <el-loading v-if="uploading" text="识别中..." :background="'rgba(0, 0, 0, 0.7)'"></el-loading>
-    <p>转换结果:{{prediction}}</p>
-    <span>{{  }}</span>
-<!--<img src='../output_img/alexandrite_18.jpg' alt="Prediction Image">-->
-<!--<img v-bind:src="'../output_img/' + predictionImagename" alt="裂开了">-->
-<!--<img v-bind:src="` vue/public/static/output_img/${predictionImagename} `" alt="裂开了">-->
-<!--<img v-bind:src="` vue/public/static/output_img/${predictionImagename} `" alt="裂开了">-->
-
-<!--    接受图片并解码-->
-    <img :src="'data:image;base64,' + image" alt="Convert the results">
-
-
-
-<!--<p>Message: {{ `vue/public/static/output_img/${predictionImagename}` }}</p>-->
-<!--    <img :src=" '../../transfers/output_img' + prediction" v-if=" '../../../transfers/output_img' + prediction" alt="prediction Image">-->
-<!--    <img :src='D:\\github\\Gem-recognition\\transfers\\output_img\\alexandrite_3.jpg' v-if="prediction" alt="prediction Image">-->
-
-<!--    <div >-->
-<!--      <div >-->
-<!--        <img :src="'data:image;base64,'+image">-->
-<!--      </div>-->
-<!--    </div>-->
+  <div id="app" class="centered-container">
+    <!-- 左侧按钮 -->
+    <div class="button-container">
+      <div class="image-placeholder" v-if="!originalImageUrl">
+        <p>上传的图片</p>
+      </div>
+      <div v-else class="image-wrapper">
+        <img :src="originalImageUrl" alt="Uploaded Image">
+      </div>
+      <div>
+        <input type="file" @change="handleFileUpload" style="display: none;">
+        <button type="button" class="btn btn-primary" onclick="document.querySelector('input[type=file]').click()">
+          选择文件
+        </button>
+      </div>
+    </div>
+    <!-- 下方选择转换风格 -->
+    <div class="style-selection">
+      <select v-model="selectedOption" @change="handleSelectChange">
+        <option disabled value="">选择要转换的风格</option>
+        <option v-for="option in options" :key="option.value" :value="option.value">
+          {{ option.label }}
+        </option>
+      </select>
+    </div>
+    <br><br>
+    <!-- 右侧按钮 -->
+    <div class="button-container">
+      <div class="image-placeholder" v-if="!image">
+        <p>转换的结果</p>
+      </div>
+      <div v-else class="image-wrapper">
+        <el-loading v-if="uploading" text="识别中..." :background="'rgba(0, 0, 0, 0.7)'"></el-loading>
+        <img :src="'data:image;base64,' + image" alt="Convert the results">
+      </div>
+      <div>
+        <button type="button" class="btn btn-primary" @click="PassInThePicture">
+          上传图片
+        </button>
+      </div>
+    </div>
+<!--  <img :src="originalImageUrl" v-if="originalImageUrl" alt="Uploaded Image">-->
+<!--  &lt;!&ndash; Loading 组件，根据 uploading 变量决定是否显示 &ndash;&gt;-->
+<!--  <el-loading v-if="uploading" text="识别中..." :background="'rgba(0, 0, 0, 0.7)'"></el-loading>-->
+<!--  <p>转换结果: {{ prediction }}</p>-->
+<!--  <img :src="'data:image;base64,' + image" alt="Convert the results">-->
   </div>
   <footer-bar></footer-bar>
 </template>
-
 <script >
 import axios from "axios";
 import HeaderBar from "@/components/HeaderBar.vue";
 import FooterBar from "@/components/FooterPage.vue";
 import {ElLoading} from "element-plus";
-
 export default {
   components: {FooterBar, HeaderBar},
   data() {
@@ -118,11 +103,9 @@ export default {
       this.uploading = true; // 开始上传时显示加载动画
       this.openFullScreen2(); // 调用显示全屏加载动画的方法
       let formData = new FormData();
-
       formData.append('image', this.file);
       // console.log(this.selectedOption)
       // formData.append('weight_file', this.weight_file);
-
       formData.append('selectedOption', this.selectedOption);
       console.log(formData.get('selectedOption'));
       axios.post('http://127.0.0.1:9300/predict2', formData,)
@@ -151,34 +134,233 @@ export default {
   }
 };
 </script>
-
-
-
 <style scoped>
 .centered-container {
   display: flex;
-  justify-content: center; /* 水平居中 */
+  flex-direction: column;
+  align-items: center;
 }
-.custom-select {
-  /* 假设你的普通按钮的样式是 40px 高，100px 宽 */
-
+.image-container {
+  text-align: center;
+}
+.image-placeholder,
+.image-wrapper {
+  border: 1px solid black;
   width: 200px;
-  height: 40px;
-  /* 可能需要覆盖 Element UI 的默认样式 */
-  .el-select__caret {
-
-    /* 调整下拉箭头的大小和位置 */
-    font-size: 16px;
-    margin-top: -8px; /* 根据需要调整 */
-  }
-  /* 其他的样式调整，如边框、圆角等 */
-  .el-select .el-input__inner {
-    height: 100%; /* 确保输入框填满整个 select 组件的高度 */
-    line-height: 40px; /* 调整文本垂直居中 */
-    padding: 0 10px; /* 调整文本内边距 */
-  }
+  height: 200px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 20px;
+}
+.image-placeholder p,
+.image-wrapper img {
+  max-width: 100%;
+  max-height: 100%;
+}
+.button-container {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 400px;
+  margin-bottom: 5px;
+}
+.button-placeholder {
+  border: 1px solid black;
+  width: 200px;
+  height: 200px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.style-selection {
+  text-align: center;
+  margin-top: 20px;
 }
 </style>
+
+
+<!--<template xmlns:v-bind="http://www.w3.org/1999/xhtml">-->
+<!--  <header-bar></header-bar>-->
+<!--&lt;!&ndash;   <div class="centered-container">&ndash;&gt;-->
+<!--&lt;!&ndash;    <el-select v-model="value" placeholder="请选择要转换的图片风格" class="custom-select"  >&ndash;&gt;-->
+<!--&lt;!&ndash;      <el-option&ndash;&gt;-->
+<!--&lt;!&ndash;        v-for="item in options"&ndash;&gt;-->
+<!--&lt;!&ndash;        :key="item.value"&ndash;&gt;-->
+<!--&lt;!&ndash;        :label="item.label"&ndash;&gt;-->
+<!--&lt;!&ndash;        :value="item.value">&ndash;&gt;-->
+<!--&lt;!&ndash;      </el-option>&ndash;&gt;-->
+<!--&lt;!&ndash;    </el-select>&ndash;&gt;-->
+<!--&lt;!&ndash;  </div>&ndash;&gt;-->
+
+
+
+
+<!--  <div  style="display: flex; justify-content: center; align-items: center; flex-direction: column;">-->
+<!--     <input type="file" @change="handleFileUpload" style="display: none;">-->
+<!--     <button type="button" class="btn btn-primary" onclick="document.querySelector('input[type=file]').click()">-->
+<!--        <span class="justify-content-center" aria-hidden="true">选择文件</span>-->
+<!--     </button>-->
+
+
+
+<!--   <div id="app" class="centered-container">-->
+<!--    <select v-model="selectedOption" @change="handleSelectChange">-->
+<!--      <option disabled value="">选择转换风格</option>-->
+<!--      <option v-for="option in options" :key="option.value" :value="option.value">-->
+<!--        {{ option.label }}-->
+<!--      </option>-->
+<!--    </select>-->
+<!--&lt;!&ndash;    <p v-if="selectedOption">当前选中的是: {{ selectedOption }}</p>&ndash;&gt;-->
+<!--  </div>-->
+
+<!--&lt;!&ndash;    <input type="file" @change="handleWeightFileUpload">&ndash;&gt;-->
+<!--    <img :src="originalImageUrl" v-if="originalImageUrl" alt="Uploaded Image">-->
+<!--    <button type="button" class="btn btn-primary" @click="PassInThePicture">上传图片</button>-->
+
+<!--    &lt;!&ndash; Loading 组件，根据 uploading 变量决定是否显示 &ndash;&gt;-->
+<!--    <el-loading v-if="uploading" text="识别中..." :background="'rgba(0, 0, 0, 0.7)'"></el-loading>-->
+<!--    <p>转换结果:{{prediction}}</p>-->
+<!--    <span>{{  }}</span>-->
+<!--&lt;!&ndash;<img src='../output_img/alexandrite_18.jpg' alt="Prediction Image">&ndash;&gt;-->
+<!--&lt;!&ndash;<img v-bind:src="'../output_img/' + predictionImagename" alt="裂开了">&ndash;&gt;-->
+<!--&lt;!&ndash;<img v-bind:src="` vue/public/static/output_img/${predictionImagename} `" alt="裂开了">&ndash;&gt;-->
+<!--&lt;!&ndash;<img v-bind:src="` vue/public/static/output_img/${predictionImagename} `" alt="裂开了">&ndash;&gt;-->
+
+<!--&lt;!&ndash;    接受图片并解码&ndash;&gt;-->
+<!--    <img :src="'data:image;base64,' + image" alt="Convert the results">-->
+
+
+
+<!--&lt;!&ndash;<p>Message: {{ `vue/public/static/output_img/${predictionImagename}` }}</p>&ndash;&gt;-->
+<!--&lt;!&ndash;    <img :src=" '../../transfers/output_img' + prediction" v-if=" '../../../transfers/output_img' + prediction" alt="prediction Image">&ndash;&gt;-->
+<!--&lt;!&ndash;    <img :src='D:\\github\\Gem-recognition\\transfers\\output_img\\alexandrite_3.jpg' v-if="prediction" alt="prediction Image">&ndash;&gt;-->
+
+<!--&lt;!&ndash;    <div >&ndash;&gt;-->
+<!--&lt;!&ndash;      <div >&ndash;&gt;-->
+<!--&lt;!&ndash;        <img :src="'data:image;base64,'+image">&ndash;&gt;-->
+<!--&lt;!&ndash;      </div>&ndash;&gt;-->
+<!--&lt;!&ndash;    </div>&ndash;&gt;-->
+<!--  </div>-->
+<!--  <footer-bar></footer-bar>-->
+<!--</template>-->
+
+<!--<script >-->
+<!--import axios from "axios";-->
+<!--import HeaderBar from "@/components/HeaderBar.vue";-->
+<!--import FooterBar from "@/components/FooterPage.vue";-->
+<!--import {ElLoading} from "element-plus";-->
+
+<!--export default {-->
+<!--  components: {FooterBar, HeaderBar},-->
+<!--  data() {-->
+<!--    return {-->
+<!--      file: null,-->
+<!--      weight_file: null,-->
+<!--      originalImageUrl:null,-->
+<!--      selectedOption: '',-->
+<!--      predictionImagename: null,-->
+<!--      prediction:"",-->
+<!--      uploading: false,-->
+<!--      jsonString:'',-->
+<!--      image: '',-->
+<!--      value: '',-->
+<!--      options: [-->
+<!--        {value: 'Option1', label: '一:毕加索'},-->
+<!--        {value: 'Option2', label: '二:七彩斑斓'},-->
+<!--        {value: 'Option3', label: '三:呐喊'},-->
+<!--        {value: 'Option4', label: '四:暴风雨'},-->
+<!--        {value: 'Option5', label: '五:皮卡比亚'},-->
+<!--        {value: 'Option6', label: '六:浮世绘'}-->
+<!--      ],-->
+<!--      formData: null,-->
+<!--    };-->
+<!--  },-->
+<!--  mounted() {-->
+<!--    // this.gain();-->
+<!--  },-->
+<!--  methods: {-->
+<!--    handleSelectChange() {-->
+<!--      // this.weight_file = this.selectedOption;-->
+<!--      // this.originalImageUrl =URL.createObjectURL(this.selectedOption);-->
+<!--      console.log('选中的值已更改:', this.selectedOption);-->
+<!--      // this.jsonString = JSON.stringify(this.selectedOption)-->
+<!--      // alert(this.weight_file);-->
+<!--    },-->
+<!--    handleFileUpload(event) {-->
+<!--      this.file = event.target.files[0];-->
+<!--      this.originalImageUrl = URL.createObjectURL(this.file);-->
+<!--    },-->
+<!--    // handleWeightFileUpload(event) {-->
+<!--    //   this.weight_file = event.target.files[0];-->
+<!--    //   // this.originalImageUrl =URL.createObjectURL(this.file);-->
+<!--    //   console.log(this.weight_file)-->
+<!--    // },-->
+<!--    PassInThePicture() {-->
+<!--      this.uploading = true; // 开始上传时显示加载动画-->
+<!--      this.openFullScreen2(); // 调用显示全屏加载动画的方法-->
+<!--      let formData = new FormData();-->
+
+<!--      formData.append('image', this.file);-->
+<!--      // console.log(this.selectedOption)-->
+<!--      // formData.append('weight_file', this.weight_file);-->
+
+<!--      formData.append('selectedOption', this.selectedOption);-->
+<!--      console.log(formData.get('selectedOption'));-->
+<!--      axios.post('http://127.0.0.1:9300/predict2', formData,)-->
+<!--          .then(response => {-->
+<!--            console.log(response);-->
+<!--            this.image = response.data.image;-->
+<!--          })-->
+<!--          .catch(error => {-->
+<!--            console.error('Error uploading image: ', error);-->
+<!--          })-->
+<!--          .finally(() => {-->
+<!--            this.closeFullScreen2(); // 接收到数据后关闭加载动画-->
+<!--            this.uploading = false;-->
+<!--          });-->
+<!--    },-->
+<!--    openFullScreen2() {-->
+<!--      ElLoading.service({-->
+<!--        lock: true,-->
+<!--        text: '识别中...',-->
+<!--        background: 'rgba(0, 0, 0, 0.7)',-->
+<!--      });-->
+<!--    },-->
+<!--    closeFullScreen2() {-->
+<!--      ElLoading.service().close();-->
+<!--    }-->
+<!--  }-->
+<!--};-->
+<!--</script>-->
+
+
+
+<!--<style scoped>-->
+<!--.centered-container {-->
+<!--  display: flex;-->
+<!--  justify-content: center; /* 水平居中 */-->
+<!--}-->
+<!--.custom-select {-->
+<!--  /* 假设你的普通按钮的样式是 40px 高，100px 宽 */-->
+
+<!--  width: 200px;-->
+<!--  height: 40px;-->
+<!--  /* 可能需要覆盖 Element UI 的默认样式 */-->
+<!--  .el-select__caret {-->
+
+<!--    /* 调整下拉箭头的大小和位置 */-->
+<!--    font-size: 16px;-->
+<!--    margin-top: -8px; /* 根据需要调整 */-->
+<!--  }-->
+<!--  /* 其他的样式调整，如边框、圆角等 */-->
+<!--  .el-select .el-input__inner {-->
+<!--    height: 100%; /* 确保输入框填满整个 select 组件的高度 */-->
+<!--    line-height: 40px; /* 调整文本垂直居中 */-->
+<!--    padding: 0 10px; /* 调整文本内边距 */-->
+<!--  }-->
+<!--}-->
+<!--</style>-->
 
 
 
